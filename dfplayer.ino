dcea6c1f614526dd785c812be2f940947f642b6d
whiterose666 <danuandreanid@gmail.com>
2020-06-10 02:29:42 +0700

    first commit

---
+++ dfplayer.ino
@@ -0,0 +1,34 @@
+
+#include<SoftwareSerial.h>
+#include<DFPlayer_Mini_Mp3.h>
+SoftwareSerial mySerial(10,11);//RX TX
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  mySerial.begin(9600);
+
+  
+mp3_set_serial(mySerial);
+delay(1);
+mp3_set_volume(35);
+}
+
+void loop() {
+  // put your main code here, torun repeatedly:
+ delay(5000);
+ if(Serial.find("ok")) {
+mp3_play(1);
+delay(5000);}
+ else if(Serial.find("apakah anda makan")) {
+mp3_play(2);
+delay(5000);}
+else{
+  mp3_play(1);
+}
+}
+
+
+
+
